3c98fc20143fe1acebe1f152d1255c9a7af4ae15
PriyanshuBhardwaj2003 <bpriyanshu57@gmail.com>
2024-02-04 21:01:15 +0530

    added player rotation

--- Cross Platform/Assets/PlayerMovement.cs
+++ Cross Platform/Assets/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,13 +9,30 @@ public class PlayerMovement : MonoBehaviour
     Vector3 movement;
     Animator animator;
     int isWalkingHash;
+    int isJumpingHash;
+    bool isGrounded;
+
+    public float rotationSpeed=200;
+
+    PlayerInputActions _input;
+    Rigidbody _rigidbody;
+
+    [SerializeField] float groundCheckDistance = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-
         isWalkingHash = Animator.StringToHash("isWalking");
+        isJumpingHash = Animator.StringToHash("isJumping");
+
+        _input = new PlayerInputActions();
+        _rigidbody = GetComponent<Rigidbody>();
+
+        // Enable jump actions
+        _input.PlayerActionMap.Enable();
+        _input.PlayerActionMap.Jump.canceled += Jump_canceled;
+        _input.PlayerActionMap.Jump.performed += Jump_performed;
     }
 
     // Update is called once per frame
@@ -25,8 +41,12 @@ public class PlayerMovement : MonoBehaviour
         // Check if the player is moving
         if (movement.magnitude > 0.1f)
         {
+            // Calculate rotation based on movement direction
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, Time.deltaTime * rotationSpeed);
+
             // Player is moving
-            transform.Translate(speed * Time.deltaTime * movement);
+            transform.Translate(speed * Time.deltaTime * Vector3.forward);
             animator.SetBool(isWalkingHash, true);
         }
         else
@@ -34,6 +54,9 @@ public class PlayerMovement : MonoBehaviour
             // Player is not moving
             animator.SetBool(isWalkingHash, false);
         }
+
+        // Perform ground check in Update
+        isGrounded = Physics.Raycast(transform.position + Vector3.up * 0.1f, -Vector3.up, groundCheckDistance);
     }
 
     public void OnMovement(InputValue val)
@@ -44,8 +67,35 @@ public class PlayerMovement : MonoBehaviour
 
     private void SetMovement(Vector2 moveVal)
     {
-        movement = moveVal;
-        movement.z = movement.y;
-        movement.y = 0;
+        movement = new Vector3(moveVal.x, 0, moveVal.y);
+    }
+
+    private void Jump_performed(InputAction.CallbackContext obj)
+    {
+        // Check if the player is grounded before allowing the jump
+        if (isGrounded)
+        {
+            _rigidbody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+
+            // Set the animator parameter to true when jump is performed
+            animator.SetBool(isJumpingHash, true);
+        }
+    }
+
+    private void Jump_canceled(InputAction.CallbackContext context)
+    {
+        var jumpOverTime = context.duration;
+        if (jumpOverTime > 1)
+        {
+            return;
+        }
+
+        if (jumpOverTime > 0.2f)
+        {
+            _rigidbody.AddForce(Vector3.up * (5f * (float)jumpOverTime), ForceMode.Impulse);
+        }
+
+        // Set the animator parameter to false when jump is canceled
+        animator.SetBool(isJumpingHash, false);
     }
 }
